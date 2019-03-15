f2e9ca063ef1912fb31c96ec7258fd4ee43adb55
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-15 05:21:51 +0000

    Added email and password columns to db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190315025636) do
+ActiveRecord::Schema.define(version: 20190315051810) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "password"
   end
 
 end
