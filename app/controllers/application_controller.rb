a477d1fb263b980c5fff503b15a982ef15afd62a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-15 06:20:43 +0000

    added user signup form and route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "thisisnotsecure"
+  end
 
   configure do
     set :public_folder, 'public'
@@ -11,6 +15,27 @@ class ApplicationController < Sinatra::Base
     #erb :welcome
     erb :index
   end
+
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+
+  post '/registrations' do
+  puts params
+  @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+  @user.save
+  session[:user_id] = @user.id
+  redirect '/users/home'
+  end
+
+  get '/users/home' do
+     @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
+
+
+
+
 
   get '/new-user' do
     erb :new_user
